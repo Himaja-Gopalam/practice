53c75610c12a68cf8c6415acc1884734aa59e9b2
Gopalam Himaja <himaja4409@gmail.com>
2026-05-28 23:35:39 +0530

    Add Calculator class with basic arithmetic operations

---
+++ Java/Basics/Calculator.java
@@ -0,0 +1,11 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        System.out.println("Add of a & b "+(a+b));
+        System.out.println("Sub of a & b "+(a-b));
+        System.out.println("Mul of a & b "+(a*b));
+    }
+}
