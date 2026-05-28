af99a39abed28d1450a5f60656b5ec4e45c45d87
Gopalam Himaja <himaja4409@gmail.com>
2026-05-28 17:57:22 +0530

    Add VariableDemo class to demonstrate variable types

---
+++ Java/Basics/VariableDemo.java
@@ -0,0 +1,19 @@
+class VariableDemo 
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        float f=10.5f;
+        double d=99.9;
+        char c = 'A';
+        boolean b= false;
+        String s="Variable demo";
+        System.out.println("The Value of Integer" +a);
+        System.out.println("The Value of float" +f);
+        System.out.println("The Value of double" +d);
+        System.out.println("The Value of character" +c);
+        System.out.println("The Value of Boolean" +b);
+        System.out.println("The Value of string" +s);
+        
+    }
+}
