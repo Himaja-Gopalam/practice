3c8e656cf4166a881af3440d6a0e529fb8c69656
Gopalam Himaja <himaja4409@gmail.com>
2026-05-29 17:28:11 +0530

    Add EvenOdd class to check positive, negative, or zero

---
+++ Java/Basics/pos_neg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        if (a>0)
+        System.out.println("positive");
+        else if (a<0)
+        System.out.println("Negative");
+        else
+        System.out.println("Zero");
+        sc.close();
+}
+}
