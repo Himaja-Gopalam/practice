c86b00080b8fb2fab68067fb3f4de9d282a43fe2
Gopalam Himaja <himaja4409@gmail.com>
2026-06-11 22:54:48 +0530

    Add patterns 3, 4, 5, 6, and 7 to pattern5.java

--- Java/Basics/pattern5.java
+++ Java/Basics/pattern5.java
@@ -21,6 +21,103 @@ class Main
             }
             System.out.println();
         }
+        \\pattern 3
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(char a='A';a<'A'+i;a++)
+            {
+                System.out.print(a);
+            }
+        
+            for(char a=(char)('A'+ i -2);a>='A';a--)
+            {
+                System.out.print(a);
+            }
+            System.out.println(" ");
+        }
+
+        //pattern4
+
+        for(int i=n;i>=1;i--)
+        {
+            for(char a=(char)('A'+i-1);a<='A'+n-1;a++)
+            {
+                System.out.print(a+" ");
+            }
+            System.out.println("");
+        }
+        //pattern 5 & 7
+        for(int i=n;i>=1;i--)
+        { 
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //pattern 6
+
+        for(int i=1;i<=n;i++)
+        { 
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
 
@@ -37,3 +134,40 @@ BB
 CCC
 DDDD
 EEEEE
+
+    A 
+   ABA 
+  ABCBA 
+ ABCDCBA 
+ABCDEDCBA 
+
+E 
+D E 
+C D E 
+B C D E 
+A B C D E
+
+**********
+****  ****
+***    ***
+**      **
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+****
+*  *
+*  *
+****
