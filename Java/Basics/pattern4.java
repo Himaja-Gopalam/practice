c409f9414dc46a120d80f390ce1b445723250716
Gopalam Himaja <himaja4409@gmail.com>
2026-07-28 17:37:40 +0530

    Update pattern4.java

--- Java/Basics/pattern4.java
+++ Java/Basics/pattern4.java
@@ -97,6 +97,196 @@ class Main
     }
 }
 
+1 to 10 patterns using functions we can find the patterns in the tuf dsa sheet
+    
+import java.util.Scanner;
+
+public class  demo 
+{
+    static void print1(int n)
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            for(int j = 0; j < n; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void print2(int n)
+    {
+        for (int i = 0; i <= n; i++) 
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void print3(int n)
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(j );
+            }
+            System.out.println();
+        }
+    }
+    static void print4(int n)
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(i );
+            }
+            System.out.println();
+        }
+    }
+    static void print5(int n)
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            for(int j = 1; j <= n
+                -i+1; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void print6(int n)
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            for(int j = 1; j <= n
+                -i+1; j++)
+            {
+                System.out.print(j );
+            }
+            System.out.println();
+        }
+    }
+    static void print7(int n)
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            for(int j = 0; j <= n-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j = 0 ; j < 2*i+1; j++)   
+            {
+                System.out.print("*");
+            }
+
+            for(int j = 0
+                ; j <= n-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void print8(int n)
+    {
+        for (int i = 0; i <= n; i++) 
+        {
+            for(int j = 1; j < i+1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j = 0 ; j < 2*n - (2*i+1); j++)   
+            {
+                System.out.print("*");
+            }
+
+            for(int j = 1; j < i+1; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void print9(int n)
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            for(int j = 1; j <= n-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j = 0 ; j < 2*i+1; j++)   
+            {
+                System.out.print("*");
+            }
+
+            for(int j = 1; j <= n-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= n; i++){
+    
+
+            for(int j = 1; j < i+1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j = 0 ; j < 2*n - (2*i+1); j++)   
+            {
+                System.out.print("*");
+            }
+
+            for(int j = 1; j <= i+1; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void print10(int n)
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i >= 1; i--) 
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();   // number of test cases
+
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            print10(n);
+        }
+        sc.close();
+    }
+}
 output:
 
 1
