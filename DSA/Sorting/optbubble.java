a108c67d6fc8b24f837821c45cbdc179b12fbc9b
Gopalam Himaja <himaja4409@gmail.com>
2026-08-04 14:09:24 +0530

    optimized version of bubble sort
    Removed the old bubble sort implementation and added an optimized bubble sort method.

--- DSA/Sorting/optbubble.java
+++ DSA/Sorting/optbubble.java
@@ -1,21 +1,6 @@
 import java.util.Scanner;
 public class bubble 
 {
-static void bubblesort(int arr[],int n)
-    {   
-        for(int i=0;i<=n-1;i++)
-        {
-            for(int j=0;j<=n-i-2;j++)
-            {
-                if(arr[j]>arr[j+1])
-                {
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
-            }
-    }
-}
 static void optbubble(int arr[],int n)
 {
     for(int i=n-1;i>=1;i--)
